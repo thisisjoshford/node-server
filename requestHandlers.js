defb6c0dd9e168312615f70c3f79429e9858ae91
Josh Ford <thisisjoshford@gmail.com>
2020-03-23 08:15:43 -0700

    upload html added

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,23 @@
 const exec = require("child_process").exec;
 
 function start(response){
+  
   console.log("Request handler 'start' was called.");
-  exec("find /",
-  { timeout: 10000, maxBuffer: 20000*1024 },
-    function (error, stdout, stderr){
+  
+    const body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
+    response.write(body);
     response.end();
   })
 }
