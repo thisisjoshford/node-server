9eb16b800b437c22a1c4f4ee432d776d348e9dcc
Josh Ford <thisisjoshford@gmail.com>
2020-03-22 20:39:54 -0700

    console log for server start and requests

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require ("http");
 
 function onRequest(request, response){
+  console.log("Request received")
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("What's up Josh?");
   response.end();
@@ -8,3 +9,4 @@ function onRequest(request, response){
 
 http.createServer(onRequest).listen(8888)
 
+console.log("Server has started")
