16a1f0768e18b59e064759eefee459d24b6d8aae
Josh Ford <thisisjoshford@gmail.com>
2020-03-22 20:47:23 -0700

    refactor onRequest into exported start function

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//use the http module in node and assign it to http (we can actually call http anything we want)
 const http = require ("http");
 
-function onRequest(request, response){
-  console.log("Request received")
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("What's up Josh?");
-  response.end();
+function start(){
+  function onRequest(request, response){
+    console.log("Request received")
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("What's up Josh?");
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888)
+  console.log("Server has started")
 }
 
-http.createServer(onRequest).listen(8888)
-
-console.log("Server has started")
+exports.start = start;
